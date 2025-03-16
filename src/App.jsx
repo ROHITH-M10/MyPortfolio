55069307e5b736cdfc55ec8c8827c2ede679789d
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-16 22:16:36 +0530

    json update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles/app.css';
 import Navbar from './components/navbar';
 import Home from './components/home';
-import About from './components/about';
+import About from './components/about'; 
 import Journey from './components/journey';
 import Projects from './components/projects';
 import Contact from './components/contact';
